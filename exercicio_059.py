0b11da490f0860609bf8c88ddd09c8de6f8553b1
Rafael Gomes <rgomessss16@gmail.com>
2025-06-24 14:28:07 -0300

    Mundo 2 Python

--- exercicio_059.py
+++ exercicio_059.py
@@ -13,7 +13,7 @@ num2 = int(input('Digite um número: '))
 sair = False
 
 while not sair:
-    menu = print('''   [1] somar
+    print('''   [1] somar
    [2] multiplicar
    [3] maior
    [4] novos números
