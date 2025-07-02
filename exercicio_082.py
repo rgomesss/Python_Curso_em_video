6129a6366b5a1da9e17a87dce2140d3f130922c4
Rafael Gomes <rgomessss16@gmail.com>
2025-07-02 20:24:09 -0300

    Mundo 3 Python

---
+++ exercicio_082.py
@@ -0,0 +1,17 @@
+#Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listar extrar que vão conter apenas os valores pares e ímpares digitados, respesctivamente. Ao final, mostre o conteúdos das três listas geradas
+
+lista1 = []
+while True:
+    lista1.append(int(input('Digite um valor: ')))
+    menu = input('Quer continuar ? [S][N]').upper()
+    if menu != 'S':
+        break
+lista_pares = []
+lista_impares = []
+for num in lista1:
+    if num % 2 == 0:
+        lista_pares.append(num)
+    else:
+        lista_impares.append(num)
+print(lista_pares)
+print(lista_impares)
