966633b018bb9f505a94bb903baf25ef346bc28a
Rafael Gomes <rgomessss16@gmail.com>
2025-07-02 17:45:06 -0300

    Mundo 3 Python

---
+++ exercicio_081.py
@@ -0,0 +1,16 @@
+#Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, mostre: a- Quantos números foram digitados; b- A lista de valores ordenada de forma decrescente; c- Se o valor 5 foi digitado e está ou não na lista
+
+lista =[]
+while True:
+    lista.append(int(input('Digite um valor: ')))
+    menu = input('Quer continuar ? [s][n]').lower()
+    if menu != 's':
+        break
+
+print(f'Você digitou: {len(lista)} números')
+lista.sort(reverse=True)
+print(lista)
+if 5 in lista:
+    print('Você digitou o numero 5')
+else:
+    print('O número 5 não está na lista')
